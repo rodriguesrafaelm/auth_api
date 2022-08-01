4f212ba627f1d0f861e4b54e8416888cbb3d4e9d
rafaelmrodriguess <rafaelmrodrigues96@yahoo.com.br>
2022-07-31 22:30:48 -0300

    add guardian to app

---
+++ lib/auth_api/authentication/guardian.ex
@@ -0,0 +1,16 @@
+defmodule AuthApi.Guardian  do
+  use Guardian, otp_app: :auth_api
+  alias AuthApi.Accounts
+
+  def subject_for_token(resource, _claims) do
+    sub = to_string(resource.id)
+    {:ok, sub}
+  end
+
+  def resources_from_claim(claims) do
+    id = claims["sub"]
+    resource = Accounts.get_by_id!(id)
+    {:ok, resource}
+  end
+
+end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule AuthApi.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:pbkdf2_elixir, "~> 2.0"}
+      {:pbkdf2_elixir, "~> 2.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
